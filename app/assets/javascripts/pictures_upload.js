becf0ba283609763cd3ead0f4bf3cd45ec029a81
Bing Xie <bing708@gmail.com>
2016-07-21 20:52:46 +1000

    Style the uploading area

--- app/assets/javascripts/pictures_upload.js
+++ app/assets/javascripts/pictures_upload.js
@@ -6,12 +6,18 @@ $(document).ready(function(){
     var dropzone = new Dropzone (".dropzone", {
       maxFilesize: 25, // set the maximum file size to 25 MB
       paramName: "picture[file_name]", // Rails expects the file upload to be something like model[field_name]
-      addRemoveLinks: false // don't show remove links on dropzone itself.
+      addRemoveLinks: false, // don't show remove links on dropzone itself.
+      // previewsContainer: ".dropzone-previews",
+      dictDefaultMessage: '拖拽图片到这里',
+      dictFileTooBig: '图片上传失败, 尺寸需小于25M',
     });
 
     dropzone.on("success", function(file) {
-      console.log(file);
-      // this.removeFile(file);
+      console.log(file.xhr.response);
+      var self = this;
+      setTimeout(function(){
+        self.removeFile(file);
+      }, 2000);
     });
   }
 });
