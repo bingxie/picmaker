1b5264fb827a8ad129d3be50639491c9b0db99f1
Bing Xie <bing708@gmail.com>
2016-08-12 18:29:47 +1000

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  devise_for :users
   mount RailsAdmin::Engine => '/helloadmin', as: 'rails_admin'
-  resources :pictures, only: [:create]
 
-  # writer your routes here
+  resources :pictures, only: [:create]
 
   mount Sidekiq::Web => '/sidekiq'
   mount StatusPage::Engine => '/'
