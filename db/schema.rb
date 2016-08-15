a202f28c80d36642afb624b701e6f5394961a6d5
Bing Xie <bing708@gmail.com>
2016-08-15 17:26:22 +1000

    Add user reference to picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812081634) do
+ActiveRecord::Schema.define(version: 20160815072019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20160812081634) do
     t.string   "exposure_time"
     t.string   "iso"
     t.string   "lens_id"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_pictures_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -69,4 +71,5 @@ ActiveRecord::Schema.define(version: 20160812081634) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "pictures", "users"
 end
